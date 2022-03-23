dafda815e47ee4e5414fff3991c58d4a977c7a63
Siyabonga Goqoza <siyabongagoqoza@gmail.com>
2022-03-23 12:22:07 +0200

    Chaotic update

--- uploadToGit.py
+++ uploadToGit.py
@@ -1,1 +1,21 @@
-print("Hello There")
+import getpass
+
+userAccount = getpass.getuser()
+
+from git import Repo
+
+path = "C:\\Users\\"+userAccount+"\\Chaos\\.git"
+PATH_OF_GIT_REPO = path  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'Chaotic update'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
+git_push()
